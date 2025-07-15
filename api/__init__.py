57d318fd2fc190d8f4e3394e2860dcfbad8740e8
gladiatordan <dholland@theboys-db.com>
2025-07-15 02:02:07 -0700

    beginning of api

---
+++ api/__init__.py
@@ -0,0 +1,37 @@
+"""
+
+Main entry point for the entire GWAPY API.
+
+
+usage: from api import GWAPY as gwapy
+
+
+"""
+from core import *
+from elevate import elevate
+
+
+
+class GWAPY(Core):
+	def __init__(self):
+		super().__init__(target_exe="gw.exe")
+		# targets = self.get_processes()
+
+		# print("Found targets for gw.exe")
+		# print(targets)
+
+	
+	def initialize(self, target_title):
+		self._initialize(target_title)
+
+
+
+def main():
+	# elevate()
+	gw = GWAPY()
+	gw.initialize("Potato Sin One")
+	
+
+
+if __name__ == "__main__":
+	main()
