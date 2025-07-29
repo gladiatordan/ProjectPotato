612c31449d2d5e569ecb27702c464204471aafec
gladiatordan <dholland@theboys-db.com>
2025-07-29 15:05:53 -0700

    fix circular imports, add temporary testing placeholder

--- api/__init__.py
+++ api/__init__.py
@@ -7,8 +7,8 @@ usage: from api import GWAPY as gwapy
 
 
 """
-from core import *
-from elevate import elevate
+from .core import *
+from .context import GameContext
 
 
 
@@ -16,22 +16,25 @@ class GWAPY(Core):
 	def __init__(self):
 		super().__init__(target_exe="Gw.exe", target_root="C:\\Users\\Daniel\\Desktop\\GW")
 		# targets = self.get_processes()
-
+		self.initialized = False
 		# print("Found targets for gw.exe")
 		# print(targets)
 
+	def _resolve_scanned_values(self):
+		rebase_delta = self.proc.base_address - self.scanner.image_base
+		print(f"REBASE DELTA -> {hex(rebase_delta)}")
+		for k, v in self.scanner.scanned_values.items():
+			# print(f"OLD ADDR FOR {k} -> {hex(v)}")
+			self.saved_values[k] = v + rebase_delta - 0x1000
+			# print(f"NEW ADDR FOR {k} -> {hex(v + rebase_delta)}")
 	
 	def initialize(self, target_title):
 		self._initialize(target_title)
+		self._resolve_scanned_values()
+		self.initialized = True
+		self.game_cxt = GameContext(self)
 
-
-
-def main():
-	# elevate()
-	gw = GWAPY()
-	gw.initialize("Potato Sin One")
-	
-
-
-if __name__ == "__main__":
-	main()
+	def get_game_cxt(self):
+		gx_struct = self.game_cxt.get_game_cxt()
+		print(type(gx_struct))
+		print(gx_struct)
